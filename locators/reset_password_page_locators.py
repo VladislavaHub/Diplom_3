3a277542a30572b03badd11d3718339268293f04
Vlada Khrustaleva <vlada.khrustaleva@bk.ru>
2024-10-31 23:20:42 +0700

    Переделано

--- locators/reset_password_page_locators.py
+++ locators/reset_password_page_locators.py
@@ -6,3 +6,5 @@ class ResetPasswordPageLocators:
     BUTTON_SHOW_AND_HIDE_PASSWORD = (By.XPATH, ".//div[contains(@class, 'icon-action')]")
     PLACEHOLDER_PASSWORD_STATUS_ACTIVE = (By.XPATH, ".//div[contains(@class, 'status_active')]")
     PLACEHOLDER_PASSWORD = (By.XPATH, ".//div[contains(@class, 'type_password')]")
+
+    status_active = 'class ="input pr-6 pl-6 input_type_password input_size_default input_status_active"'
--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ import allure
 from selenium.common import TimeoutException
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+from seletools.actions import drag_and_drop
 
 
 class BasePage:
@@ -22,9 +23,9 @@ class BasePage:
     def click_element(self, locator):
         self.find_element(locator).click()
 
-    @allure.step('Ожидание исчезновения элемента')
-    def wait_element_invisibility_of_element(self, timeout, locator):
-        WebDriverWait(self.driver, timeout).until(expected_conditions.invisibility_of_element_located(locator))
+    @allure.step('Ожидание наличия элемента на странице')
+    def wait_element_presence_of_element_located(self, timeout, locator):
+        WebDriverWait(self.driver, timeout).until(expected_conditions.presence_of_element_located(locator))
 
     @allure.step('Ожидание кликабельности элемента')
     def wait_element_element_to_be_clickable(self, timeout, locator):
@@ -34,7 +35,15 @@ class BasePage:
     def wait_element_visibility_of_element(self, timeout, locator):
         WebDriverWait(self.driver, timeout).until(expected_conditions.visibility_of_element_located(locator))
 
-    @allure.step('Просмотр текста элемента')
+    @allure.step('Ожидание исчезновения элемента')
+    def wait_element_invisibility_of_element(self, timeout, locator):
+        WebDriverWait(self.driver, timeout).until(expected_conditions.invisibility_of_element_located(locator))
+
+    @allure.step('Неявное ожидание')
+    def wait(self, locator, text):
+        WebDriverWait(self.driver, 10).until(expected_conditions.text_to_be_present_in_element(locator, text))
+
+    @allure.step('Получение текста элемента')
     def get_text(self, locator):
         return self.find_element(locator).text
 
@@ -46,7 +55,7 @@ class BasePage:
     def send_keys(self, locator, text):
         self.find_element(locator).send_keys(text)
 
-    @allure.step('Просмотр элемента')
+    @allure.step('Видимость элемента')
     def element_visibility(self, locator):
         result = True
         try:
@@ -54,3 +63,9 @@ class BasePage:
         except TimeoutException:
             result = False
         return result
+
+    @allure.step('Перетаскивание Элемента')
+    def drag_and_drop_elemen(self, source, target):
+        source = self.find_element(source)
+        target = self.find_element(target)
+        return drag_and_drop(self.driver, source, target)
